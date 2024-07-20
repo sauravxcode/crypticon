e3e258f366daa57f270ea3a85784953ee7d34d4e
sachin <sachinsharma11175@gmail.com>
2024-07-20 20:13:44 +0530

    update

--- includes/game.php
+++ includes/game.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Gaming Interface</title>
-    <link rel="stylesheet" href="game2.css">
+    <link rel="stylesheet" href="game.css">
     <link href="https://fonts.googleapis.com/css2?family=Orbitron:wght@400;700&display=swap" rel="stylesheet">
 </head>
 <body>
--- login.php
+++ login.php
@@ -1,14 +1,13 @@
 <!DOCTYPE html>
-<html lang="en" >
+<html lang="en">
 <head>
   <meta charset="UTF-8">
   <title>CodePen - Mouse Orbit</title>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/modernizr/2.8.3/modernizr.min.js" type="text/javascript"></script>
   <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"/>
-
-<meta name="viewport" content="width=device-width, initial-scale=1"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
-<link rel="stylesheet" href="login.css">
-
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/meyer-reset/2.0/reset.min.css">
+  <link rel="stylesheet" href="login.css">
 </head>
 <body>   
 <div id="container">
@@ -18,7 +17,7 @@
         <div class="text">
            USER LOGIN
         </div>
-        <form>
+        <form id="login-form">
            <div class="field">
               <div class="fas fa-envelope"></div>
               <input type="text" placeholder="Email or Phone">
@@ -27,18 +26,24 @@
               <div class="fas fa-lock"></div>
               <input type="password" placeholder="Password">
            </div>
-           <button>LOGIN 🡥</button>
+           <button type="submit" id="login-game">LOGIN 🡥</button>
            <div class="link">
               Have a Question?
-              <a href="#">Join Discord</a>
+              <a href="https://www.google.com">Join Discord</a>
            </div>
         </form>
      </div>
     </div>
 </div>
 <!-- partial -->
-  <script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-<script src='//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js'></script><script  src="login.js"></script>
-
+<script>
+document.getElementById("login-form").addEventListener("submit", (event) => {
+    event.preventDefault();
+    window.location.href = "../includes/game.php";
+});
+</script>
+<script src='//cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
+<script src='//cdnjs.cloudflare.com/ajax/libs/lodash.js/2.4.1/lodash.min.js'></script>
+<script src="login.js"></script>
 </body>
 </html>
